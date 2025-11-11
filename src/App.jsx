b1e2332b3ec8568c70658db352888a3fb0fd7870
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-11 11:51:44 +0100

    create movies list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+
 function App() {
+
+  const movies = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ];
 
   return (
     <>
-      
+      <div className="container">
+        <div className="card">
+          <ul className="list-group">
+            {
+              movies.map((movie) => (
+                <li className="list-group-item">
+                  <h4>{movie.title}</h4> 
+                  <small>{movie.genre}</small>
+                </li>
+              ))
+            }
+          </ul>
+          
+        </div>
+      </div>
     </>
   )
 }
